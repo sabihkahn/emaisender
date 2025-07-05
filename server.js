01374c53202e65f72fadd33cb6c72ac5d0d61189
sabihkahn <whyonlyhead@gmail.com>
2025-07-05 14:37:49 +0500

    yes

---
+++ server.js
@@ -0,0 +1,49 @@
+//
+import express from 'express';
+
+import nodemailer from 'nodemailer';
+const app = express();
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+
+
+// Replace these with your actual Gmail and App Password
+const senderEmail = 'whyonlyhead@gmail.com';
+const appPassword = 'atob hjie fujb shdd'; // 🔐 Use your 16-char app password here
+
+app.post('/send-email', (req, res) => {
+  const { name,email,message } = req.body;
+
+
+  const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: senderEmail,
+    pass: appPassword,
+  },
+});
+
+const mailOptions = { 
+  from: senderEmail,
+  to: 'sabihop56@gmail.com', // ✅ change this to who you want to email
+  subject: name + ' has sent you a message',
+  text: message+"\n\nFrom: " + name + "\nEmail: " + email,
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('❌ Error sending email:', error);
+  } else {
+    console.log('✅ Email sent:', info.response);
+  }
+});
+res.status(200).json({sucess:true,
+message: 'Email sent successfully!' });
+
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+} );
